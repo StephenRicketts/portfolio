da53a4d5d269d870343c398b2b32d9a98cc46fb4
StephenRicketts <stephen.ricketts12@gmail.com>
2021-02-25 21:23:00 -0500

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar(props) {
+  return (
+    <div className="flex-row p-6 text-green-300 bg-gray-900 border-green-300 border-solid border-2 content-center">
+      <h1 className="hover:text-white">Stephen Ricketts</h1>
+    </div>
+  );
+}
+
+export default NavBar;
