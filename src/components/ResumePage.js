b22b6b189c40285ffa95f1068af97323c6a9467d
StephenRicketts <stephen.ricketts12@gmail.com>
2021-04-11 16:26:49 -0400

    added education

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDoubleDown,
+  faAngleDoubleUp,
+} from "@fortawesome/free-solid-svg-icons";
+import FadeInAnimation from "./animations/FadeIn";
 
 export default function ResumePage() {
+  const [diplomaToggle, setDiplomaToggle] = useState(false);
+
+  const icon = () => {
+    diplomaToggle ? (
+      <FontAwesomeIcon className="pl-2" icon={faAngleDoubleUp} />
+    ) : (
+      <FontAwesomeIcon className="pl-2" icon={faAngleDoubleDown} />
+    );
+  };
   return (
     <div className="text-center text-gray-300">
       <h1 className="pt-14 pb-2 text-2xl">Stephen Ricketts</h1>
@@ -49,6 +64,52 @@ export default function ResumePage() {
       <h2 className="text-3xl pb-10  mx-auto  w-1/5 border-b-2 border-green-300">
         Education
       </h2>
+      <h2 className="text-2xl py-8">
+        Diploma in Full-Stack Development - Concordia University 2019
+      </h2>
+      <div className=" mb-24 mx-auto md:w-1/2 lg:w-1/3 text-justify space-y-4">
+        <p>
+          Completed 480+ hours of intensive full-stack web development course
+          focused on programming fundamentals including test-driven development,
+          object-oriented programming, MVC, SCRUM, Agile methodologies,
+          pair-programming, algorithms and databases, etc.
+        </p>
+        <div className="text-center">
+          <button
+            onClick={() => {
+              setDiplomaToggle(!diplomaToggle);
+            }}
+            className="my-8 py-2 px-4 border-2 border-green-300 text-green-300 hover:bg-green-300 hover:text-black"
+          >
+            View Diploma
+          </button>
+          {diplomaToggle && (
+            <FadeInAnimation
+              direction="down"
+              className="py-8 px-4"
+              distance={100}
+            >
+              <img
+                className="mx-auto"
+                src="/images/diploma.jpg"
+                alt="diploma"
+              />
+            </FadeInAnimation>
+          )}
+        </div>
+        <h2 className="text-2xl text-center">
+          React Native - The Practical Guide <br /> [2020 edition]
+        </h2>
+        <p>
+          This course covers using React Native to build native iOS and Android
+          Apps - incl. Push Notifications, Hooks, Redux. (32 hours of lectures,
+          4 projects)
+        </p>
+        View Certificate{" "}
+        <a href="https://www.udemy.com/certificate/UC-c5e16da5-af95-4246-a4e6-f89131bb84a3/ ">
+          here
+        </a>
+      </div>
     </div>
   );
 }
